96ae7264d20389383c8ab32b3dff8f0f33f0302e
BrandonPilane <BrandonPilane@gmail.com>
2025-09-07 17:31:25 +0200

    Question 1

--- q1.c
+++ q1.c
@@ -1,8 +1,19 @@
+// Question: Write a program that asks the user to enter a number and uses an
+// if–else statement to check if it is even or odd.
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
-  printf("Hello world");
+  printf("Enter a number: ");
+  int num;
+  scanf("%d", &num);
+
+  if (num % 2 == 0) {
+    printf("%d is even\n", num);
+  } else {
+    printf("%d is odd\n", num);
+  }
 
   return EXIT_SUCCESS;
 }
