54108926b2d86372fe053b7eb6c338d9c68e8cd7
BrandonPilane <BrandonPilane@gmail.com>
2025-09-10 01:50:54 +0200

    Question 4

--- q4.c
+++ q4.c
@@ -1,8 +1,22 @@
+// Write a program that inputs age and checks using an if statement if the
+// person is eligible to vote (age ≥ 18).
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
-  printf("Hello world");
+  int age;
+
+  printf("Enter your age: ");
+  scanf("%d", &age);
+
+  if (age < 18 && age > 0) {
+    printf("\nYou are not eligible to vote at %d\n", age);
+  } else if (age >= 18) {
+    printf("\nYou can vote!\n");
+  } else {
+    printf("\nHuh?\n");
+  }
 
   return EXIT_SUCCESS;
 }
